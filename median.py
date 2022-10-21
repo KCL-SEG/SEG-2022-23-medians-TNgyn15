3b5d896484ac605f0a78e5de5192055a7f26e158
Thang Nguyen <thang.1.nguyen@kcl.ac.uk>
2022-10-21 11:29:24 +0100

    Add Solution

--- median.py
+++ median.py
@@ -1,5 +1,4 @@
-"""Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
+"Median calculator."
 
 while True:
     try:
@@ -9,4 +8,14 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+numbers.sort()
+
+if len(numbers) % 2 == 0:
+    index = int(len(numbers) / 2)
+    median = (numbers[index - 1] + numbers[index]) / 2
+else:
+    index = int((len(numbers) - 1) / 2)
+    median = numbers[index]
+
+print(median)
